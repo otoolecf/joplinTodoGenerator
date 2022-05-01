e67f187df446934dceb79f5fc8d8d7d415a37e6c
otoolecf <otoolecf@gmail.com>
2022-05-01 15:06:54 -0700

    only try to create todos on note changes to avoid partial lines. add option to ignore timestamps.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
 import joplin from 'api';
+import JoplinSettings from 'api/JoplinSettings';
 import { time, timeEnd } from 'console';
+
+const TODO_TITLE = "Todos";
+const keywords = ["TODO", "TIP", "IDEA"];
+const special_command_marker = "//";
+const ignore_timestamp_command = "NO TIMESTAMP";
+let last_line = {content: undefined, index: undefined};
 
 joplin.plugins.register({
 	onStart: async function() {
 		console.info('Hello world. Test plugin started!');
-		async function addTimestamp() {
+		async function logicHandler() {
 			try {
 				const note = await joplin.workspace.selectedNote();
-				if (note) {
+				console.log("logicHandler: note.title: ", note.title);
+				if (note && note.title !== TODO_TITLE) {
 					// console.log("note found. Note id: ", note.id);
-					let last_header = findLastTimeHeader(note.body);
-					console.log("addTimestamp: Last timestamp header: ", last_header);
-					let new_timestamp = generateTimestamp(last_header);
-					// console.log("generateTimestamp result: ", new_timestamp);
+					let new_timestamp = await addTimestamp(note.body)
 					if (new_timestamp) {
 						console.log("time for new timestamp! appending to note...")
 						await apppendToNote(note.id, note.body, new_timestamp, true);
+					}
+					let todo_items = findKeywords(note.body);
+					console.log("logicHandler: found keywords: ", todo_items);
+					todo_items = todo_items.filter(item => item.keyword === "TODO");
+					console.log("logicHandler: found todos: ", todo_items);
+					if (todo_items && todo_items.length) {
+						console.log("logicHandler: creating todos!");
+						for (let i=0; i<todo_items.length; i++) {
+							let todo = todo_items[i];
+							await createTodo(note.id, todo.line, todo.pos);
+						}
 					}
  				}
 			} catch(err){
@@ -23,22 +39,72 @@ joplin.plugins.register({
 			}
 		}
 		await joplin.workspace.onNoteSelectionChange(() => {
-			addTimestamp();
+			//fires when new note selected
+			logicHandler();
 		});
-		addTimestamp();
+		await joplin.workspace.onNoteChange(async () => {
+			// runs when note changes
+			let note_changed = await noteChangeChecker();
+			if (note_changed) {
+				logicHandler();
+			}
+		});
+		//runs when plugin starts
+		logicHandler();
 
 	},
 });
+
+
+async function noteChangeChecker() {
+	// returns true if the current note has changed significantly (newline, etc)
+	let note_changed = false;
+	const note = await joplin.workspace.selectedNote();
+	let lines = note.body.split('\n');
+	if (lines && lines.length) {
+		let current_line_index = lines.length - 1;
+		let current_line = lines.pop();
+		if (last_line.content && last_line.index) {
+			if (last_line.index !== current_line_index && !current_line.includes(last_line.content)) {
+				console.log("noteChangeChecker: significant change detected!");
+				note_changed = true;
+			}
+		}
+		last_line.content = current_line;
+		last_line.index = current_line_index;
+	}
+	return note_changed;
+}
+
+async function addTimestamp(note_body) {
+	// check if special handler sequence exists in body
+	// if so, execute commands
+	let lines = note_body.split('\n');
+	let special_lines = lines.filter(line => line.includes(special_command_marker));
+	console.log("addTimestamp: special lines: ", special_lines);
+	if (special_lines) {
+		let ignore_command = special_lines.find(line => line.includes(ignore_timestamp_command));
+		if (ignore_command) {
+			console.log("command found: ", ignore_command);
+			return;
+		}
+	}
+	let last_header_obj = findLastTimeHeader(note_body);
+	console.log("addTimestamp: Last timestamp header: ", last_header_obj);
+	let last_header;
+	if (last_header_obj && last_header_obj.header) last_header = last_header_obj.header
+	let new_timestamp = generateTimestamp(last_header);
+	// console.log("generateTimestamp result: ", new_timestamp);
+	return new_timestamp;
+}
 
 function findLastTimeHeader(note_body) {
 	let time_headers = [];
 	let lines = note_body.split('\n');
 	if(!lines || !lines.length) return;
-	lines.forEach(line => {
-		let header;
+	lines.forEach((line,i) => {
 		if (line && line.match(/===/)) {
-			header = line;
-			time_headers.push(header);
+			time_headers.push({header: line, pos: i})
 		}
 	});
 	return time_headers.pop();
@@ -110,7 +176,12 @@ function convertDateFromString(str) {
 async function apppendToNote(id, note_body, append_str, current_note) {
 	try {
 		// Set the note body
+		// If append_str already included, DO NOT APPEND
 		console.log("appendToNote noteId: ", id, ", str: ",append_str);
+		if (note_body.includes(append_str)) {
+			console.log("Note already includes append_str.");
+			return;
+		}
 		let full_body = note_body + "\n" + append_str;
 		await joplin.data.put(['notes', id], null, { body: full_body });
 		// need to setText to update editor UI, current bug, see joplin #5955 on github
@@ -123,3 +194,51 @@ async function apppendToNote(id, note_body, append_str, current_note) {
 		console.log("ERROR: appendToNote: ", err);
 	}
 }
+
+function findKeywords(note_body, start_idx = 0) {
+	// Finds keywords added after start_idx. If not provided, start_idx = 0;
+	let keyword_instances = [];
+	let lines = note_body.split('\n');
+	if(lines && lines.length && start_idx < lines.length) {
+		lines = lines.slice(start_idx);
+		lines.forEach((line, l_idx) => {
+			keywords.forEach(kw => {
+				if (line.match(kw)) keyword_instances.push({keyword: kw, line: line, pos: start_idx + l_idx});
+			});
+		});
+	}
+	return keyword_instances;
+}
+
+
+async function createTodo(src_id, todo_text, todo_idx) {
+	try {
+		let todo_note;
+		let append_str = todo_text + " || src_note_id: " + src_id + " , src_idx: " + todo_idx;
+		let existing_notes = await joplin.data.get(['notes'], {query: TODO_TITLE, fields: ['id', 'title', 'body']});
+		existing_notes = existing_notes.items;
+		console.log("createTodo: data GET result: ", existing_notes);
+		if (existing_notes && existing_notes.length) {
+			todo_note = existing_notes.find(n => n.title === TODO_TITLE);
+			console.log("todo_note: ", todo_note);
+		} 
+		if (todo_note) {
+			if (!todo_note.body.includes(todo_text)) {
+				await apppendToNote(todo_note.id, todo_note.body, append_str, false);
+			} else {
+				console.log("createTodo: Todo list already includes todo_text: ", todo_text);
+			}
+		} else {
+			//create the todo
+			console.log("createTodo: no todo_note found. Creating new note!");
+			let new_todos_params = {"title": TODO_TITLE, "body": append_str};
+			await joplin.data.post(['notes'], null, new_todos_params);
+		}
+		//TODO: Link the todos to proper note
+		//TODO: cross out old todos
+		return;
+	} catch(err) {
+		console.log("ERROR: createTodo: ", err.message);
+	}
+}
+
